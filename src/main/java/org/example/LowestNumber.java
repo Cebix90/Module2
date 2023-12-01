c68f5b049459b396264501e1388612729c242e7c
Cebix90 <cebulmat@gmail.com>
2023-12-01 23:33:57 +0100

    LowestNumber refactor

--- src/main/java/org/example/LowestNumber.java
+++ src/main/java/org/example/LowestNumber.java
@@ -5,38 +5,28 @@ import java.util.List;
 import java.util.Scanner;
 
 public class LowestNumber {
-    private final Scanner scanner = new Scanner(System.in);
-    private final List<Double> numbers = new ArrayList<>();
+    public void lowestOfFiveNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = new ArrayList<>();
 
-    public void run() {
-        getANumbers();
-        if (!numbers.isEmpty())
-            findTheLowestNumber();
-        else
-            System.out.println("List of numbers is empty.");
-    }
+        System.out.println("Enter 5 numbers and get the lowest:");
 
-    private void findTheLowestNumber() {
-        double lowest = numbers.get(0);
+        while(numbers.size() < 5) {
+            if(scanner.hasNextInt()){
+                int input = scanner.nextInt();
+                numbers.add(input);
+            } else {
+                System.out.println("Invalid input. Please enter a valid number.");
+                scanner.next();
+            }
+        }
+
+        int lowest = numbers.get(0);
         for(int i = 1; i < numbers.size(); i++) {
             if (numbers.get(i) < lowest)
                 lowest = numbers.get(i);
         }
 
         System.out.println("The lowest number given " + lowest);
-    }
-    private void getANumbers() {
-        System.out.println("Enter 5 numbers and get the lowest:");
-
-        for (int i = 0; i < 5; i++) {
-            try {
-                double input = Double.parseDouble(scanner.nextLine());
-                numbers.add(input);
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please enter a valid number.");
-                i--;
-            }
-        }
-        scanner.close();
     }
 }
